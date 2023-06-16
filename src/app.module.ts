aaccdae44fd0f4369fd351bb78c36cdcf283215b
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2023-06-16 02:09:44 -0400

    Generating modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { ServicesModule } from './services/services.module';
+import { ControllersModule } from './controllers/controllers.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, ServicesModule, ControllersModule],
   controllers: [AppController],
   providers: [AppService],
 })
