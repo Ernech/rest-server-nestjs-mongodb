e46e17aa704a9e26f7f0ac7ef11b4f662cd23b3c
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2023-06-16 22:07:15 -0400

    Creating a user schema

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+
+    @Prop({required:[true,'Name is required']})
+    name:string;
+
+    @Prop({required:[true,'Email is required']})
+    email:string;
+
+    @Prop({required:[true,'Password is required']})
+    password:string;
+
+    @Prop({required:true, enum:['ADMIN_ROLE', 'USER_ROLE']})
+    role:string
+
+    @Prop()
+    img:string;
+
+    @Prop()
+    state:boolean;
+
+}
+
+
+export const UserSchema = SchemaFactory.createForClass(User);
