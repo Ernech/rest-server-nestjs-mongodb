b1548081e4a03b8db9780d532e4ca685cf9191da
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2023-06-22 23:01:32 -0400

    Set guards into guards module

--- src/guards/guards.module.ts
+++ src/guards/guards.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD, APP_PIPE } from '@nestjs/core';
 import { ServicesModule } from 'src/services/services.module';
+import { TokenGuard } from './token.guard';
+import { RoleGuard } from './role.guard';
+import { TrimPipe } from './trim.pipe.guard';
 
 @Module({
   imports: [ServicesModule],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: TokenGuard
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard
+    },
+    {
+      provide: APP_PIPE,
+      useClass: TrimPipe
+    }
+  ]
 })
 export class GuardsModule {}
